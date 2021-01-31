67c2ce51a784b977936d1278ffe9f8dc8d9d8488
ChangHyun <jchangh2@gmail.com>
2021-02-01 01:02:42 +0900

    fix : 이모션 css prop 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 const Test = styled.div`
   color: red;
 `;
+
 export default function App() {
   return (
     <>
-      <h1>assdsdsssdsdssdsd</h1>
+      <h1
+        css={css`
+          color: orange;
+        `}
+      >
+        assdsdsssdsdssdsd
+      </h1>
       <Test>hello</Test>
     </>
   );
